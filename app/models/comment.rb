ed46b3107d363e3b2f2f09b9bb98c68664f1bda7
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-08-11 05:04:52 -0600

    Add comment count increment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
     belongs_to :author, class_name: 'User'
     belongs_to :post, class_name: 'Post'
+
+    after_save :update_comments_count
+
+  private
+
+  def update_comments_count
+    post.increment!(:comments_count)
+  end
   end
