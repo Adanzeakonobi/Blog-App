23bbcff7d51215f813d54051b0b6886f81df67ae
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-08-11 16:31:05 -0600

    Added unit spec for users

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'User validations' do
+    subject { User.new(name: 'Ada', photo: 'ada.png', bio: 'Student.', posts_counter: 0) }
+
+    before { subject.save }
+
+    it 'Check name should be present' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'User should have post greater than or equal to 0' do
+      subject.posts_counter = 1
+      expect(subject).to be_valid
+    end
+
+    it 'posts_counter should be integer' do
+      subject.posts_counter = 'text'
+      expect(subject).to_not be_valid
+    end
+
+    # it ':most_recent_posts should return 3 recent posts' do
+    #   3.times { Post.create(author: subject, title: 'Title1', text: 'This is a post') }
+    #   expect(subject.most_recent_posts.length).to eq(3)
+    # end
+  end
+
+  describe 'Should test recent post method' do
+    before { 4.times { |post| Post.create(author: subject, title: "my first post") } }
+
+    it 'User should have three recent posts' do
+      expect(subject.most_recent_posts).to eq(subject.posts.last(3))
+    end
+  end
+end
