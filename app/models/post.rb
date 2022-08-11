c494ead813b26f7c0c8495679fc7aafffe4dd883
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-08-11 05:18:04 -0600

    Add posts count increment

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,16 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: 'User'
     has_many :likes, foreign_key: 'post_id'
     has_many :comments, foreign_key: 'post_id'
+
+    after_save :update_posts_count
+
+    def most_recent_comments
+      comments.order(created_at: :desc).limit(5)
+    end
+
+  private
+
+  def update_posts_count
+    post.increment!(:posts_count)
+  end
   end
