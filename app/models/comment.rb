ef0df9407a77832a22094bd0660e47343ab55002
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-08-11 06:24:00 -0600

    Update/correct linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,12 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :post, class_name: 'Post'
+  belongs_to :author, class_name: 'User'
+  belongs_to :post, class_name: 'Post'
 
-    after_save :update_comments_count
+  after_save :update_comments_count
 
   private
 
   def update_comments_count
     post.increment!(:comments_count)
   end
-  end
+end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,12 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :post, class_name: 'Post'
+  belongs_to :author, class_name: 'User'
+  belongs_to :post, class_name: 'Post'
 
-    after_save :update_likes_count
+  after_save :update_likes_count
 
   private
 
   def update_likes_count
     post.increment!(:likes_count)
   end
-  end
+end
