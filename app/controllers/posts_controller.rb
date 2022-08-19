ebd9bbc2c555995b18cd1ceb597baaec30b4e1ad
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-08-19 09:10:23 -0600

    Add new and create actions in the post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,18 @@
 class PostsController < ApplicationController
+  include ApplicationHelper
+
+  def create
+    @post = Post.new(params.require(:post).permit(:author_id, :title, :text))
+
+    if @post.save
+      flash[:notice] = 'Post saved successfully'
+      redirect_to user_path(@post.author_id)
+    else
+      flash[:error] = 'Post not saved'
+      redirect_to new_user_post_path(@post.author_id)
+    end
+  end
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.order(id: :desc)
@@ -6,6 +20,11 @@ class PostsController < ApplicationController
     redirect_to users_path
   end
 
+  def new
+    p current_user
+    @post = Post.new(author_id: current_user.id)
+  end
+  
   def show
     @post = Post.find(params[:id])
   rescue ActiveRecord::RecordNotFound
