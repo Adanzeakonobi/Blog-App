254a32a00a1c2509d044a1b2cf9d6f2d5ea97c6f
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-08-19 09:33:29 -0600

    Add additional routes for comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: %i[create index new show] do
+      resources :comments, only: %i[create new]
+      resources :likes, only: %i[create]
   end
 end
