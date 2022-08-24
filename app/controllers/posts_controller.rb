007d51f8f809d1fc38666fa62ba3164e9ece7f35
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-08-24 06:57:01 -0600

    Update post controller index method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.includes([:author])
+    @posts = @user.posts.order(id: :desc)
   rescue ActiveRecord::RecordNotFound
     redirect_to users_path
   end
