b616095e6e028e44cd5b5498f4d99b33d591c946
marurevi <maryrev@hotmail.com>
2022-08-25 09:00:07 -0300

    Modify users integration test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :request do
+  subject(:author) do
+    user = User.new(name: 'Adanna', photo: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
+                    bio: 'Public Administator.', email: 'viviluv.20147@gmail.com')
+    user.password = 'valido'
+    user.password_confirmation = 'valido'
+    user.confirm
+    user
+  end
+
+  before(:each) do
+    sign_in(author)
+  end
+
+  after(:each) do
+    sign_out(author)
+  end
+
   describe 'when client displays all users (#index)' do
     before(:each) { get users_path }
 
@@ -18,7 +35,7 @@ RSpec.describe UsersController, type: :request do
   end
 
   describe 'when client displays a single user (#show)' do
-    subject(:author) { User.create(name: 'Adanna', photo: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png', bio: 'Public Administator.') }
+    # subject(:author) { User.create(name: 'Adanna', photo: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png', bio: 'Public Administator.') }
 
     before(:each) { get "/users/#{author.id}" }
 
